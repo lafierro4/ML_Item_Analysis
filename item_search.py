ab34ca3e9417ede6cb018b5a7af427aec0aa977c
dlcastaneda <dlcastaneda@miners.utep.edu>
2024-04-30 23:46:48 -0600

    Refractor API connection and error handling

--- item_search.py
+++ item_search.py
@@ -1,37 +1,41 @@
 import requests
 import json
 import pandas as pd
-import DataProcessor 
+from DataProcessor import items_data
 version = "14.8.1" #change to get specific version data, follow 14.x.1 guide, latest available version 14.7.1
 language = "en_US"
 
 #connecting to the the api
-try:
-    data_url = f"https://ddragon.leagueoflegends.com/cdn/{version}/data/{language}/item.json"
-    data_response = requests.get(data_url)
-    print(data_response.status_code)
-except ConnectionError as error:
-    print("Connection Error: ", error) 
+def connect(version, language):
+    try:
+        data_url = f"https://ddragon.leagueoflegends.com/cdn/{version}/data/{language}/item.json"
+        data_response = requests.get(data_url)
+        data_response.raise_for_status() #raise an exception for any HTTP errors
+        return data_response.json()
+    except requests.exceptions.RequestException as e:#ConnectionError as error:
+        print("Connection Error: ", e) 
+        return None
 
 #getting the json from the response
-items_data = data_response.json()
+items_data = connect(version, language)
 
 # seperate items, only getting items that are usuable to the project and get all the needed individual item infomation  
-items_list = []
-if "data" in items_data:
-    for item_id, item in items_data["data"].items():
-        if all([not item.get(key, False) for key in ["requiredChampion", "requiredAlly"]]) and \
-           item.get("maps", {}).get("11", False) and \
-           item.get("gold", {}).get("purchasable", False) and \
-           not item.get("hideFromAll", False):
-            stats = item.get("stats", {})
-            if stats: #if the item doesnt have statWs skip it,
-                items_list.append({
-                    "item_id": item_id,
-                    "name": item.get("name", ""),
-                    "gold": item.get("gold", {}),
-                    "stats": stats
-                })
+if items_data:
+    items_list = []
+    if "data" in items_data:
+        for item_id, item in items_data["data"].items():
+            if all([not item.get(key, False) for key in ["requiredChampion", "requiredAlly"]]) and \
+            item.get("maps", {}).get("11", False) and \
+            item.get("gold", {}).get("purchasable", False) and \
+            not item.get("hideFromAll", False):
+                stats = item.get("stats", {})
+                if stats: #if the item doesnt have statWs skip it,
+                    items_list.append({
+                        "item_id": item_id,
+                        "name": item.get("name", ""),
+                        "gold": item.get("gold", {}),
+                        "stats": stats
+                    })
 #making json file with all of the item's infomation for refernce
 with open(f"Item Data/{version}_item_data.json", 'w') as data_file:
     output_data = {"item_count": len(items_list), "items": items_list}
